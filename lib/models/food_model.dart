204c3f70d1319c85340cc66cee1db07a482f7453
MussaK22 <2022124@miu.edu.mn>
2026-03-19 14:14:13 +0800

    food screen

--- lib/models/food_model.dart
+++ lib/models/food_model.dart
@@ -1,11 +1,17 @@
-class Food {
-
+class FoodModel {
   final String name;
   final int calories;
+  final double protein; // in grams
+  final double carbs;   // in grams
+  final double fat;     // in grams
+  final String imageUrl;
 
-  Food({
+  FoodModel({
     required this.name,
     required this.calories,
+    required this.protein,
+    required this.carbs,
+    required this.fat,
+    this.imageUrl = '',
   });
-
 }
