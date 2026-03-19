9733a466e3432ceab176ed04ed2f8030341afa63
MussaK22 <2022124@miu.edu.mn>
2026-03-19 14:20:24 +0800

    add all missing files

--- lib/widgets/progress_card.dart
+++ lib/widgets/progress_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ProgressCard extends StatelessWidget {
-
   final String title;
   final String value;
 
@@ -14,20 +13,32 @@ class ProgressCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(20),
+      width: MediaQuery.of(context).size.width * 0.4,
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
+        color: Colors.white,
         borderRadius: BorderRadius.circular(20),
-        color: Colors.deepPurple.withOpacity(0.3),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            blurRadius: 10,
+            spreadRadius: 5,
+          )
+        ],
       ),
       child: Column(
         children: [
-          Text(title),
-          const SizedBox(height: 10),
+          Text(
+            title,
+            style: const TextStyle(color: Colors.grey, fontSize: 14),
+          ),
+          const SizedBox(height: 8),
           Text(
             value,
             style: const TextStyle(
-              fontSize: 22,
+              fontSize: 24,
               fontWeight: FontWeight.bold,
+              color: Colors.deepPurple,
             ),
           ),
         ],
